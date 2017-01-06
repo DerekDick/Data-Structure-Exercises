47d902ed331ed416ebcb2e6b88b46a90f2b19247
Jianfeng Hou <derekwangyi@163.com>
2017-01-06 14:17:12 +0800

    Create CircularLinkedList.hpp

---
+++ Exercise02/CircularLinkedList.hpp
@@ -0,0 +1,87 @@
+#ifndef _CIRCULAR_LINKED_LIST_HPP_
+#define _CIRCULAR_LINKED_LIST_HPP_
+
+#include "Node.hpp"
+
+template <class T>
+class CircularLinkedList {
+public:
+	// Function members
+	CircularLinkedList() {
+		/*
+		The constructor
+		Initialize _head and _tail
+		*/
+
+		_head = NULL;
+		_tail = NULL;
+	}
+
+	~CircularLinkedList() {
+		/*
+		The destrustor
+		Release the memory allocated for every node
+		*/
+
+		if (_head) {
+			Node<T>* current = _head;
+			Node<T>* temp = NULL;
+			do {
+				temp = current;
+				current = current->getNext();
+				delete temp;
+			} while (current != _head);
+		}
+	}
+
+	Node<T>* getHead(void) {
+		return _head;
+	}
+
+	Node<T>* getTail(void) {
+		return _tail;
+	}
+
+	T at(int index) {
+		/* Get the data of the node at the certain index of the linked list */
+		Node<T>* current = _head;
+		for (int i = 0; i < index; i++) {
+			current = current->getNext();
+		}
+		return current->getData();
+	}
+
+	void push_back(T & data) {
+		if (!_head) {
+			Node<T>* newNode = new Node<T>(data);
+			newNode->setNext(newNode);
+			_head = _tail = newNode;
+		}
+		else {
+			Node<T>* newNode = new Node<T>(data);
+			_tail->setNext(newNode);
+			newNode->setNext(_head);
+			_tail = newNode;
+		}
+	}
+
+	Node<T>& operator[] (int index) {
+		return getDataAt(index);
+	}
+
+private:
+	// Data members
+	Node<T>* _head;
+	Node<T>* _tail;
+
+	// Function members
+	Node<T>& getDataAt(int index) {
+		Node<T>* current = _head;
+		for (int i = 0; i < index; i++) {
+			current = current->getNext();
+		}
+		return *current;
+	}
+};
+
+#endif // _CIRCULAR_LINKED_LIST_HPP_
